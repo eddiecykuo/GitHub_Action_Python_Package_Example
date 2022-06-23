db9f890a3ea80e9fc5b0324b77afc8ab730c57bb
eddiecykuo <60646295+eddiecykuo@users.noreply.github.com>
2022-06-23 09:27:46 +0800

    Update client.py

--- GitHub_Action_Python_Example/client.py
+++ GitHub_Action_Python_Example/client.py
@@ -14,4 +14,4 @@ class HelloWorld:
     headers = {"Authorization": "Bearer " + token, "Content-Type" : "application/x-www-form-urlencoded"}
     payload = {'message': msg }
     r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
-    return r.status_code
+        return r.status_code
