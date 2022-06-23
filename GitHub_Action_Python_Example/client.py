f76aec6d8fcfd8c378e9ba3b5afa13ec7603fbb3
eddiecykuo <60646295+eddiecykuo@users.noreply.github.com>
2022-06-23 09:27:29 +0800

    Update client.py

--- GitHub_Action_Python_Example/client.py
+++ GitHub_Action_Python_Example/client.py
@@ -9,3 +9,9 @@ class HelloWorld:
 
     def get_message(self):
         return self.message
+    
+    def lineNotifyMessage(token, msg):          #傳送lineNotify
+    headers = {"Authorization": "Bearer " + token, "Content-Type" : "application/x-www-form-urlencoded"}
+    payload = {'message': msg }
+    r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
+    return r.status_code
